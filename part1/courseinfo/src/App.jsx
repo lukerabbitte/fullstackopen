163041f47db90be5b5fc6cb9c12de3aa597f8988
Luke Rabbitte <lrabbitt@tcd.ie>
2023-09-25 15:52:52 +0100

    Exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development';
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={parts[0]} />
-      <Content part={parts[1]} />
-      <Content part={parts[2]} />
-      <Count parts={parts} />
+      <Header course={course.name} />
+      <Content part={course.parts[0]} />
+      <Content part={course.parts[1]} />
+      <Content part={course.parts[2]} />
+      <Count parts={course.parts} />
     </div>
   )
 }
